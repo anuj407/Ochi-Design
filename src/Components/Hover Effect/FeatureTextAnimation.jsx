a5e6e8f7337fd92a46f1a6899d6ef407725a363a
anuj407 <anuj97953a@gmail.com>
2024-09-01 18:04:19 +0530

    text Animation

--- src/Components/Hover Effect/FeatureTextAnimation.jsx
+++ src/Components/Hover Effect/FeatureTextAnimation.jsx
@@ -2,7 +2,7 @@
 import { motion } from "framer-motion"
 function FeatureTextAnimation({title,animate,index}) {
   return (
-            <h1 className={`text-[17vw] h-[16vw] text-[#EC6234] lg:absolute lg:left-1/2 lg:top-1/2 lg:-translate-x-[50%] lg:-translate-y-[50%] lg:text-[#cdea68] font-semibold font-['Founders_Grotesk_X_Condensed'] uppercase lg:text-[8vw] lg:leading-[8.5vw] lg:h-[5.5vw] flex overflow-hidden `}>
+            <h1 className={`text-[4rem] leading-[4.5rem] h-[3rem] text-[#EC6234] lg:absolute lg:left-1/2 lg:top-1/2 lg:-translate-x-[50%] lg:-translate-y-[50%] lg:text-[#cdea68] font-semibold font-['Founders_Grotesk_X_Condensed'] uppercase lg:text-[8vw] lg:leading-[8.5vw] lg:h-[5.5vw] flex overflow-hidden `}>
               {title.split("").map((items, indexs) => (    
                 <motion.span initial={{y:"100%"}} animate={animate[index]} transition={{ease:[0.22,1,0.36,1], delay:indexs*0.05}} className=" inline-block" key={indexs}>{items}</motion.span>       
               ))}
